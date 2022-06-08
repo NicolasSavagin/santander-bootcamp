57d19326c4c4f2706569fd8c5459fab1db1485b7
NicolasSavagin <nicolascezar19@gmail.com>
2022-06-07 21:29:12 -0300

    santander-bootcamp

---
+++ moduloII-html-css3-js/Introducao-ao-javascript/To-do-list/assets/scripts.js
@@ -0,0 +1,29 @@
+const taskList = document.getElementById('tarefas');
+const form = document.getElementById('formulario');
+
+form.onsubmit = function (e){
+    e.preventDefault();
+    const inputField = document.getElementById('escrever');
+    addTask(inputField.value);
+    form.reset(); 
+};
+
+function addTask(description) {
+	const taskContainer = document.createElement('div');
+	const newTask = document.createElement('input');
+	const taskLabel = document.createElement('label');
+	const taskDescriptionNode = document.createTextNode(description);
+
+	newTask.setAttribute('type', 'checkbox');
+	newTask.setAttribute('name', description);
+	newTask.setAttribute('id', description);
+
+	taskLabel.setAttribute('for', description);
+	taskLabel.appendChild(taskDescriptionNode);
+
+	taskContainer.classList.add('task-item');
+	taskContainer.appendChild(newTask);
+	taskContainer.appendChild(taskLabel);
+
+	taskList.appendChild(taskContainer);
+}
